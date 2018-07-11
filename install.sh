93537c3246341261dc40559b50ad2fcfd7d70be8
William Bradley <wfbradley@cmtelematics.com>
2018-07-10 21:40:09 -0400

    Adding in requirements.txt part

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt-get -y install python
 sudo apt -y install python-pip
 sudo apt -y install cython
+pip install -r requirements.txt --user --upgrade
 curl http://scheet.org/code/Linuxfp.tar.gz --output Linuxfp.tar.gz
 tar -xvzf Linuxfp.tar.gz
 chmod a+x fastPHASE
