6e0221a90f6c28ca1173355d90f1ea2baba518d2
William Bradley <wfbradley@cmtelematics.com>
2018-12-29 15:50:19 -0500

    Flushing log buffers when halting

--- halt_machine.py
+++ halt_machine.py
@@ -1,5 +1,6 @@
 import utils_snpko as utils
 import subprocess
+import logging
 
 logger = utils.logger
 
@@ -7,4 +8,6 @@ logger = utils.logger
 def possibly_halt(args):
     if args.halt:
         logger.info("Halting machine.")
+        # Flush any log buffers
+        logging.shutdown()
         subprocess.call('sudo shutdown -h now', shell=True)
